e70e0f1821a09252514044e7c8c7c06520a84b7b
rokodios <angeltorresnavarro91@gmail.com>
2024-06-05 08:32:30 +0000

    v2 ubuntu vm

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-## Cluster K8s ##
+## Variables VMs Ubuntu ##
 module "master_01" {
  source      = "./modules/vm-cloud-init-ubuntu"
   name_host   = "master01"
@@ -8,4 +8,26 @@ module "master_01" {
   cpu_cores   = "2"
   vm_hdd_size = "50"
   vm_ip_v4    = "192.168.1.210"
+}
+
+module "worker_01" {
+ source      = "./modules/vm-cloud-init-ubuntu"
+ name_host   = "worker01"
+ id_vm       = "302"
+ vm_ram      = "2048"
+ cpu_sockets = "1"
+ cpu_cores   = "2"
+ vm_hdd_size = "50"
+ vm_ip_v4    = "192.168.1.211"
+}
+
+module "worker_02" {
+ source      = "./modules/vm-cloud-init-ubuntu"
+ name_host   = "worker02"
+ id_vm       = "303"
+ vm_ram      = "2048"
+ cpu_sockets = "1"
+ cpu_cores   = "2"
+ vm_hdd_size = "50"
+ vm_ip_v4    = "192.168.1.212"
 }
