ef161b24c82a26cede4429a35cacb697587fc9c4
rokodios <134306990+rokodios@users.noreply.github.com>
2024-10-03 14:37:44 +0200

    add opnsense

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-## Module VM creation ##
+## Module VM creation Generic Cloud init ##
 module "vm_creation" {
  source      = "./modules/vm-creation-module"
  vms_module = var.vms
@@ -6,3 +6,15 @@ module "vm_creation" {
  vm_pass_module = var.vm_pass
 }
 
+## OPNsense specific VM ##
+
+resource "proxmox_vm_qemu" "opnsense" {
+     name_host        = "fw-core-01"
+     domain_host      = "devopso.lab"
+     id_vm            = 200
+     node_env         = "proxmox"
+     template_clone   = "template-opnsense"
+     full_clone       = true
+     qemu_agent       = 0
+     hhd_storage_pool = "storage-vm"
+   }
